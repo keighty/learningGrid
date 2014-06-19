090220b271422c57ee2077abca31b20ba1d038e5
keighty <k80.leonard@gmail.com>
2014-06-19 07:47:40 -0700

    Add button category helper to dry out item template.

--- client/main.js
+++ client/main.js
@@ -3,3 +3,9 @@ Accounts.ui.config({ passwordSignupFields: 'USERNAME_AND_EMAIL' });
 Handlebars.registerHelper('formatDate', function(date) {
   return $.datepicker.formatDate('dd-M-yy', date);
 });
+
+Handlebars.registerHelper('btnClass', function(category) {
+  if(category === 'got-it') return "btn-primary";
+  if(category === 'learn-it') return "btn-warning";
+  if(category === 'one-day') return "btn-inverse";
+});
