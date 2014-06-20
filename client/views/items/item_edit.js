2d92201e1756d11ad7164f6453d33bafbf46771b
keighty <k80.leonard@gmail.com>
2014-06-20 07:58:10 -0700

    Add editItem form and functions.

---
+++ client/views/items/item_edit.js
@@ -0,0 +1,33 @@
+Template.itemEdit.events({
+  'submit form': function(e) {
+    e.preventDefault();
+
+    var currentItemId = this._id;
+    var itemProperties = {
+      title: $(e.target).find('[name=title]').val(),
+      description: $(e.target).find('[name=description]').val(),
+      category: $(e.target).find('[name=category]').val(),
+    };
+
+    Items.update(currentItemId, { $set: itemProperties }, function(error) {
+      if(error)
+        alert(e.reason);
+      else
+        Router.go('itemPage', {_id: currentItemId});
+    });
+  },
+  'click .delete': function(e) {
+    e.preventDefault();
+    if(confirm("Delete this item?")) {
+      var currentItemId = this._id;
+      Items.remove(currentItemId);
+      Router.go('itemsList');
+    }
+  }
+});
+
+Template.itemEdit.helpers({
+  isSelected: function(value) {
+    return value === this.category;
+  }
+});
