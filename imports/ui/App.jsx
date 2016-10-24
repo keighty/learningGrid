499aa7076466549834573a5b4874f660811b115e
keighty <k80.leonard@gmail.com>
2016-10-23 20:54:39 -0700

    Add form field.

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,22 +1,54 @@
 import React, { Component, PropTypes } from 'react'
+import ReactDOM from 'react-dom'
 import { createContainer } from 'meteor/react-meteor-data'
 
 import { Items } from '../api/items.js'
 import Item from './Item.jsx'
 
-class App extends Component {
 
+class App extends Component {
   renderItems(category) {
     return this.props.items
               .filter(item => item.category === category)
               .map(item => <Item key={item._id} item={item} />)
   }
 
+  handleSubmit(e) {
+    e.preventDefault()
+    const {textInput, categorySelect} = this.refs
+    const node1 = ReactDOM.findDOMNode(textInput)
+    const node2 = ReactDOM.findDOMNode(categorySelect)
+
+    const text = node1.value.trim()
+    const category = node2.value
+
+    Items.insert({
+      text,
+      category,
+      createdAt: new Date(),
+    })
+
+    node1.value = ''
+  }
+
   render() {
+    const options = [ 'one', 'two', 'three' ]
+
     return (
       <div className='container'>
         <header className='u-full-width'>
           <h1>Learning Grid</h1>
+          <form className="new-task" onSubmit={this.handleSubmit.bind(this)} >
+            <select ref="categorySelect">
+              <option value="comfort">Comfort Zone</option>
+              <option value="learning">Learning Zone</option>
+              <option value="terror">Terror Zone</option>
+            </select>
+            <input
+              type="text"
+              ref="textInput"
+              placeholder="Type to add new items" />
+          </form>
         </header>
         <div className='row'>
           <div className='one-third column comfort'>
